44f4da77fae8718b83ede84b6afac308e562b4bc
ckj <2637376981@qq.com>
2026-03-05 17:17:33 +0800

    使用langchain实现与Deepseek的首次对话

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from ai-agent-test!")
-
-
+from langchain_ollama.chat_models import ChatOllama
 if __name__ == "__main__":
-    main()
+    llm=ChatOllama(model="deepseek-r1:7b")
+    messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+    resp=llm.invoke(messages)
+    print(resp)
